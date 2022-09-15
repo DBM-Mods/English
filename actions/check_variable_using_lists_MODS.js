0c9d7904416d8c53c21a8b4535bd78958d83810a
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-09-14 23:34:29 -0300

    Update check_variable_using_lists_MODS.js

--- actions/check_variable_using_lists_MODS.js
+++ actions/check_variable_using_lists_MODS.js
@@ -3,8 +3,8 @@ module.exports = {
   name: "Check Variable Using Lists MOD",
   section: "Conditions",
   meta: {
-    version: '2.1.5',
-    preciseCheck: true,
+    version: '2.1.6',
+    preciseCheck: false,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/English',
     downloadURL: 'https://github.com/DBM-Mods/English/archive/refs/heads/main.zip',
