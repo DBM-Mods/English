febed76e6e26df06e67545235a48b9664c3822c2
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-09-14 23:34:01 -0300

    Update canvas_draw_text_MODS.js

--- actions/canvas_draw_text_MODS.js
+++ actions/canvas_draw_text_MODS.js
@@ -3,8 +3,8 @@ module.exports = {
 name: "Canvas Draw Text on Image MOD",
 section: "Image Editing",
 meta: {
-    version: '2.1.5',
-    preciseCheck: true,
+    version: '2.1.6',
+    preciseCheck: false,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/English',
     downloadURL: 'https://github.com/DBM-Mods/English/archive/refs/heads/main.zip',
